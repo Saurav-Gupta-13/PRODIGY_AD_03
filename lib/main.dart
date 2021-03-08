1c4f848faa5fc70b3d22b093201cb9eb6fd7ed54
Muhammad Daffa' A.R <daffavcd@gmail.com>
2021-03-08 16:09:42 +0700

    buat tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool mulai = false;
+  void klik_mulai() {
+    this.mulai = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,6 +36,52 @@ class MyApp extends StatelessWidget {
                 margin: const EdgeInsets.only(top: 250.0),
                 child: Text("00:00:00",
                     style: TextStyle(fontSize: 55, color: Colors.black54)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () => klik_mulai(),
+                    child: const Text('Play', style: TextStyle(fontSize: 20)),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    elevation: 5,
+                  ),
+                  SizedBox(width: 30.0),
+                  RaisedButton(
+                    onPressed: () => klik_mulai(),
+                    child: const Text('Lap', style: TextStyle(fontSize: 20)),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    elevation: 5,
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Text("Historic lap",
+                    style: TextStyle(fontSize: 40, color: Colors.black54)),
+              ),
+              SizedBox(width: 10.0),
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: Text("01   +00:00:91    00:05:53",
+                          style:
+                              TextStyle(fontSize: 25, color: Colors.black54)),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: Text("02   +00:05:65    00:05:34",
+                          style:
+                              TextStyle(fontSize: 25, color: Colors.black54)),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
